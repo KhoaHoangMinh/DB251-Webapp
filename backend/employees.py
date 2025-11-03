7f790f6baceede4dfb41289ec899a7e09edd4363
TVTruong777 <vinhtruong060705@gmail.com>
2025-11-03 23:30:27 +0700

    update employees.py

--- backend/employees.py
+++ backend/employees.py
@@ -8,12 +8,17 @@ class Employee(BaseModel):
     id: int
     name: str
     email: str
-    phone: int
+    phone: str
+
+class Employees_Create(BaseModel):
+    name: str
+    email: str
+    phone: str
 
 employee_db = {
-    1 : Employee(id=1, name="khoa", email="khoa@example.com", phone=12345678),
-    2 : Employee(id=1, name="john", email="john@example.com", phone=12345678),
-    3 : Employee(id=1, name="duc", email="duc@example.com", phone=12345678),
+    1 : Employee(id=1, name="khoa", email="khoa@example.com", phone="12345678"),
+    2 : Employee(id=2, name="john", email="john@example.com", phone="12345678"),
+    3 : Employee(id=3, name="duc", email="duc@example.com", phone="12345678"),
 }
 next_id = 3
 
@@ -28,17 +33,50 @@ def view_employee(employee_id: int):
         raise  HTTPException(status_code=404, detail="employee not found")
     return employee
 
+# @router.post("/", status_code=status.HTTP_201_CREATED)
+# def create_employee(id: int, name: str, email: str, phone: int):
+#     global next_id
+#     if id < 0 or phone < 0:
+#         raise HTTPException(status_code=400, detail="Invalid id or phone number")
+#     new_employee = Employee(id=id, name=name, email=email, phone=phone)
+#     employee_db[next_id] = new_employee
+#     next_id += 1
+#     return new_employee
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_employee(id: int, name: str, email: str, phone: int):
+def create_employee(employee: Employees_Create):
     global next_id
-    if id < 0 or phone < 0:
-        raise HTTPException(status_code=400, detail="Invalid id or phone number")
-    new_employee = Employee(id=id, name=name, email=email, phone=phone)
+    if len(employee.phone) != 10 or employee.phone[0] != '0':
+        raise HTTPException(status_code=400, detail="Invalid phone number")
+    print("DEBUG:", employee.email)
+    if "@gmail.com" not in employee.email:
+        raise HTTPException(status_code=400, detail="Invalid email")
+
+    new_employee = Employee(
+        id=next_id,
+        name=employee.name,
+        email=employee.email,
+        phone=employee.phone
+    )
+
     employee_db[next_id] = new_employee
     next_id += 1
     return new_employee
 
 @router.put("/{employee_id}")
+def update_employee(employee_id: int,
+                    name: Optional[str] = None,
+                    email: Optional[str] = None,
+                    phone: Optional[str] = None):
+    employee = employee_db.get(employee_id)
+    if not employee:
+        raise HTTPException(status_code=404, detail="employee not found")
+    if name: employee.name = name
+    if email: employee.email = email
+    if phone: employee.phone = phone
+    return employee
+
+@router.put("/")
 def update_employee(employee_id: int,
                     name: Optional[str] = None,
                     email: Optional[str] = None,
