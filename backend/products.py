3d1999c718b71b57c67b7c842372c69fa4fafb5a
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-11-12 08:03:50 +0700

    use real database in products

--- backend/products.py
+++ backend/products.py
@@ -1,124 +1,89 @@
 from fastapi import APIRouter, HTTPException, status
 from typing import Optional, Dict, List, Annotated
-from fastapi.params import Query
 from pydantic import BaseModel
 
 from database import Base, engine, get_db, db_dependency
-import models
+from models import Product
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
-class Product(BaseModel):
-    id: int
-    name: str
-    category: str
-    price: float
-    stock: int
-
 class ProductUpdate(BaseModel):
-    id: int
-    name: Optional[str] = None
-    category: Optional[str] = None
-    price: Optional[float] = None
-    stock: Optional[int] = None
+    ProductName: Optional[str] = None
+    ProductDescription: Optional[str] = None
 
 class ProductCreate(BaseModel):
     ProductName: str
     ProductDescription: str
 
-class StockUpdate(BaseModel):
-    stock: int
-
 class SummaryStats(BaseModel):
     total_products: int
-    total_stock: int
-    avg_price: float
 
-product_db: Dict[int, Product] = {
-    1 : Product(id=1, name="Macbook", category="Electronics", price=1200.0, stock=15),
-    2 : Product(id=2, name="Smartphone", category="Electronics", price=800.0, stock=30),
-    3 : Product(id=3, name="Desk Chair", category="Furniture", price=150.0, stock=10),
-}
-next_id = 4
-
-@router.get("/", response_model=List[Product])
-def list_products() -> List[Product]:
-    return list(product_db.values())
+@router.get("/")
+def list_products(db: db_dependency):
+    return db.query(Product).all()
 
 @router.get("/stats", response_model=SummaryStats)
-def get_summary_stats() -> SummaryStats:
-    total_products = len(product_db)
+def get_summary_stats(db: db_dependency) -> SummaryStats:
+    total_products = len(db.query(Product).all())
     if total_products == 0:
-        return SummaryStats(total_products=0, total_stocks=0, avg_price=0.0)
-    total_stock = sum(product.stock for product in product_db.values())
-    avg_price = round(sum(product.price for product in product_db.values())/total_products, 2)
-    return SummaryStats(total_products=total_products, total_stock=total_stock, avg_price=avg_price)
+        return SummaryStats(total_products=0)
+    return SummaryStats(total_products=total_products)
 
-@router.get("/search", response_model=List[Product])
-def search_product(name: Optional[str] = Query(None),
-                   category: Optional[str] = Query(None)
-                   ) -> List[Product]:
-    products = list(product_db.values())
-    if name:
-        products = [p for p in products if name.lower() in p.name.lower()]
-    elif category:
-        products = [p for p in products if category.lower() in p.category.lower()]
+def search_cond(a, b):
+    return a.lower() in b.ProductName.lower() or a.lower() in b.ProductDescription.lower()
+@router.get("/search")
+def search_product(search: str, db: db_dependency):
+    products = list(db.query(Product).all())
+    products = [p for p in products if search_cond(search, p)]
     return products
 
-@router.get("/{product_id}", response_model=Product)
-def view_product(product_id: int) -> List[Product]:
-    product = product_db.get(product_id)
+@router.get("/{product_id}")
+def view_product(product_id: int, db: db_dependency):
+    product = db.query(Product).get(product_id)
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
     return product
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 def create_product(product: ProductCreate, db: db_dependency):
-    new_product = models.Product(**product.dict())
+    new_product = Product(**product.dict())
     db.add(new_product)
     db.commit()
+    db.refresh(new_product)
+    return new_product
 
-@router.post("/create_bulk", response_model=List[Product], status_code=status.HTTP_201_CREATED)
-def create_products(products: List[ProductCreate]) -> List[Product]:
+@router.post("/create_bulk", status_code=status.HTTP_201_CREATED)
+def create_products(products: List[ProductCreate], db: db_dependency):
     created = []
     for product in products:
-        created.append(create_product(product))
+        created.append(create_product(product, db))
     return created
 
-@router.put("/{product_id}", response_model=Product)
-def update_product(product_update: ProductUpdate) -> Product:
-    product = product_db.get(product_update.id)
+@router.put("/{product_id}")
+def update_product(product_id: int, product_update: ProductUpdate, db: db_dependency):
+    product = db.query(Product).filter(Product.ProductID == product_id).first()
     if not product:
         raise HTTPException(status_code=404, detail="Invalid product ID")
-    if product_update.name: product.name = product_update.name
-    if product_update.category: product.category = product_update.category
-    if product_update.price: product.price = product_update.price
-    if product_update.stock: product.stock = product_update.stock
-    return product
-
-@router.patch("/{product_id}/stock", response_model=Product)
-def update_qty(product_id: int, stock_update: StockUpdate) -> Product:
-    product = product_db.get(product_id)
-    if not product:
-        raise HTTPException(status_code=404, detail="Invalid product ID")
-    if stock_update.stock < 0:
-        raise HTTPException(status_code=400, detail="Negative stock qty")
-    product.stock = stock_update.stock
-    product_db[product_id] = product
+    if product_update.ProductName: product.ProductName = product_update.ProductName
+    if product_update.ProductDescription: product.ProductDescription = product_update.ProductDescription
+    db.commit()
+    db.refresh(product)
     return product
 
 @router.delete("/")
-def delete_bulk(indexes: List[int]):
+def delete_bulk(indexes: List[int], db: db_dependency):
     for index in indexes:
-        delete_product(index)
+        delete_product(index, db)
     return {"message" : "success"}
 
 @router.delete("/{product_id}")
-def delete_product(product_id: int):
-    if not product_db.get(product_id):
+def delete_product(product_id: int, db: db_dependency):
+    product = db.query(Product).filter(Product.ProductID == product_id).first()
+    if not product:
         raise HTTPException(status_code=404, detail="Invalid product ID")
-    product_db.pop(product_id)
+    db.delete(product)
+    db.commit()
     return {"message": f"Product {product_id} deleted successfully"}
 
 
