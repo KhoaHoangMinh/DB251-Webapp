571e008f64c417b749ba9857398a24202d03ff11
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-11-14 07:58:57 +0700

    fix computed value error

--- backend/models.py
+++ backend/models.py
@@ -120,7 +120,7 @@ class OrderItem(Base):
     ProductID = Column(String(10), ForeignKey('Product.ProductID'), primary_key=True)
     Quantity = Column(Integer, nullable=False)
     UnitPrice = Column(DECIMAL(10, 2), nullable=False)
-    LineTotal = Column(DECIMAL(10, 2), computed='Quantity * UnitPrice')
+    LineTotal = Column(DECIMAL(10, 2))
 
     # Relationships
     order = relationship("Orders", back_populates="order_items")
